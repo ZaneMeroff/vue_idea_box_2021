1e225896469c68c3dd74b9c797e9993a221860a1
ZaneMeroff <zanemeroff@gmail.com>
2021-01-11 23:44:26 -0700

    Remove unused code in Idea.vue

--- src/components/Idea.vue
+++ src/components/Idea.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="idea-outer-container">
 
-    <button @click="handleDelete">X</button>
-    <p>{{ id }}</p>
+    <button @click="handleDelete" class="delete-btn">X</button>
     <p>{{ title }}</p>
     <p>{{ description }}</p>
 
@@ -17,19 +16,11 @@
       title:        { type: String, default: "" },
       description:  { type: String, default: "" },
     },
-    data() {
-      return {
-
-      }
-    },
     methods: {
 
       handleDelete() {
         this.$emit("delete", this.id)
       },
-    },
-    mounted() {
-
     },
   }
 </script>
@@ -37,10 +28,28 @@
 <style scoped>
 
   .idea-outer-container {
-    border: 2px solid #000;
+    background-color: #fff;
+    box-shadow: 5px 10px 18px #bbbbbb;
     height: 150px;
     width: 150px;
     text-align: center;
+    margin: 10px;
+    border-radius: 5px;
+  }
+
+  .delete-btn {
+    background-color: #ffc1c1;
+    border: none;
+    height: 20px;
+    width: 20px;
+    border-radius: 10px;
+    padding-top: 2px;
+    margin: 7px 0px 0px 115px;
+  }
+
+  .delete-btn:hover {
+    cursor: pointer;
+    background-color: #ff4b4b;
   }
 
 </style>
