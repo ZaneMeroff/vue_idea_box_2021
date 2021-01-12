9f70ce58a72c7818b4e252cce82397846f047679
ZaneMeroff <zanemeroff@gmail.com>
2021-01-11 23:19:19 -0700

    Create Idea structure

---
+++ src/components/Idea.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="idea-outer-container">
+
+    <button @click="handleDelete">X</button>
+    <p>{{ id }}</p>
+    <p>{{ title }}</p>
+    <p>{{ description }}</p>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "IdeasContainer",
+    props: {
+      id:           { type: Number, default: 0 },
+      title:        { type: String, default: "" },
+      description:  { type: String, default: "" },
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+      handleDelete() {
+        this.$emit("delete", this.id)
+      },
+    },
+    mounted() {
+
+    },
+  }
+</script>
+
+<style scoped>
+
+  .idea-outer-container {
+    border: 2px solid #000;
+    height: 150px;
+    width: 150px;
+    text-align: center;
+  }
+
+</style>
