51a02b5dc72fb800bc0ecc0eb436b8ded2bc5462
ZaneMeroff <zanemeroff@gmail.com>
2021-01-11 22:24:20 -0700

    Create basic IdeaContainer structure

---
+++ src/components/IdeasContainer.vue
@@ -0,0 +1,34 @@
+<template>
+  <form class="ideas-outer-container">
+
+  </form>
+</template>
+
+<script>
+  export default {
+    name: "IdeasContainer",
+    props: [],
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    },
+  }
+</script>
+
+<style scoped>
+
+  .ideas-outer-container {
+    border: 2px solid blue;
+    height: 100%;
+    width: 100%;
+    text-align: center;
+  }
+
+</style>
